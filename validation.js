1413d89a6e60c5938765ab490eb02ac790f96fbf
Gaurav Lodha <gauravlodha77@gmail.com>
2018-02-11 19:58:36 +0530

    updated validation

--- validation.js
+++ validation.js
@@ -9,4 +9,15 @@ function validateForm(){
         alert('You must agree to the terms first.');
         return false;
     }
+    var obj1 = document.getElementById('text1');
+    if(trimfield(obj1.value) == '') 
+    {      
+         alert("Please Provide Details!");
+         obj1.focus();
+         return false;       
+    }
+}
+function trimfield(str) 
+{ 
+    return str.replace(/^\s+|\s+$/g,''); 
 }
